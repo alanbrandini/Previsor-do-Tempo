778c579f19261fe74ab8c1599e427b2458154d0c
alanbrandini <alanbrandini@gmail.com>
2017-01-18 16:01:08 -0200

    Add files via upload
    Commit do código do sensor de movimento.

---
+++ projetofinal.ino
@@ -0,0 +1,16 @@
+int sensor = 5;
+int movimento = LOW; 
+
+void setup() {
+  pinMode(sensor, INPUT); 
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  movimento = digitalRead(sensor);  
+  if (movimento == HIGH){
+    Serial.println("ola");
+  }
+
+}
